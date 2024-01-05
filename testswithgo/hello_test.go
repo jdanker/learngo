5fe74f3cca691f9e650a6f125ad223a580529a28
Jahred Danker <jahrede@gmail.com>
2024-01-05 15:28:02 -0600

    refactor

--- testswithgo/hello_test.go
+++ testswithgo/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jahred")
-	want := "Hello, Jahred"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jahred")
+		want := "Hello, Jahred"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+	t.Run("say 'Hello, World' when empty string supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
