df3960cc8c75db8ad48ee05bc7defa4c77619074
Jahred Danker <jahrede@gmail.com>
2024-01-10 14:08:41 -0600

    completed tdd of hello world

--- testswithgo/hello.go
+++ testswithgo/hello.go
@@ -2,12 +2,35 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 
-func Hello(name string) string {
-	return englishHelloPrefix + name
+	french  = "French"
+	spanish = "Spanish"
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {  // named return val
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Jahred"))
+	fmt.Println(Hello("Jahred", "English"))
 }
