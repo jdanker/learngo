dbebbe36745d2fa30a6bc1e88ce03ecee2cf33c3
Jahred Danker <jahrede@gmail.com>
2024-01-11 15:51:06 -0600

    slices/structs

--- testswithgo/sum/sum_test.go
+++ testswithgo/sum/sum_test.go
@@ -1,20 +1,47 @@
 package sum
 
 import (
+	"slices"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1,2,3,4,5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
-	
+
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("tail of 2 slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+
 	})
 
 }
